128681b4940d662261dda53caa3d02781c885474
Mark Bucciarelli <mkbucc@gmail.com>
2016-08-27 10:51:05 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # interest_calculator
-Given starting balance, ending balance and number of months calculates monthly and annual interest rate.
+Given starting balance, ending balance and number of months calculates monthly
+and annual interest rate.
+
+Requires the bc utility.
+
+Usage example:
+
+	$ ./interest_calculator.sh
+	interest_calculator.sh <STARTING_BALANCE> <ENDING_BALANCE> <NUMBER_OF_MONTHS>
+	$ ./interest_calculator.sh 94.76 99.53 13
+	Starting balance = 94.76
+	Ending balance   = 99.53
+	Number of months = 13
+	    --> change in principle    =     4.77
+		monthly interest rate  =     0.38%
+		annual interest rate   =     4.6%
+	$
---
+++ interest_calculator.sh
@@ -0,0 +1,45 @@
+#! /bin/sh -e
+# Compute monthly and annual compound interest rate.
+# Created on 8/27/2016 by Mark Bucciarelli <mkbucc@gmail.com>
+
+usage() {
+	printf "$(basename $0) <STARTING_BALANCE> <ENDING_BALANCE> <NUMBER_OF_MONTHS>\n" >&2
+	exit 1
+}
+
+[ "x$1" = "x" ] && usage
+[ "x$2" = "x" ] && usage
+[ "x$3" = "x" ] && usage
+
+STARTING_BALANCE=$1
+ENDING_BALANCE=$2
+NUMBER_OF_MONTHS=$3
+
+#
+#		rate = (end/start)^(1/n) - 1
+#
+#	bc does not handle non-integer exponents. But its exponential function
+#	does, so we need to use the relation
+#
+#		x^y = e^(y * ln(x))
+#
+#	to calculate interest rate like so:
+#
+#		rate	= (end/start)^(1/n)
+#
+#			= e( (1/n) * ln(end/start) )
+#
+
+DELTA=$(echo "scale=5; $ENDING_BALANCE - $STARTING_BALANCE" | bc)
+M=$(echo "scale=5; e((1/$NUMBER_OF_MONTHS) * l($ENDING_BALANCE/$STARTING_BALANCE)) - 1" |bc -l)
+Y=$(echo "scale=5; (1 + $M)^12 - 1" | bc)
+
+MPERCENT=$(echo "scale=1; $M*100" |bc)
+YPERCENT=$(echo "scale=1; $Y*100" |bc)
+
+printf "Starting balance = %.2f\n" $STARTING_BALANCE
+printf "Ending balance   = %.2f\n" $ENDING_BALANCE
+printf "Number of months = %d\n" $NUMBER_OF_MONTHS
+printf "    --> change in principle    = %12.2f\n" $DELTA
+printf "        monthly interest rate  = %12.2f%%\n" $MPERCENT
+printf "        annual interest rate   = %11.1f%%\n" $YPERCENT
